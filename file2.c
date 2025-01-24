92db65aab5114a7c607f46121146686db23e9e55
b-zebrowski <6879017+b-zebrowski@users.noreply.github.com>
2025-01-24 08:28:56 -0500

    Update file2.c

--- file2.c
+++ file2.c
@@ -1,9 +1,9 @@
-// This is a single line comment, you should not see the link to https://github.com/accountnotexist/some-repo
+// This is a single line comment, you should not see the link to https://github.com/b-zebrowski/1-deep
 
 /*
-Now lets check a multi-line comment, you should not see a link to https://github.com/accountnotexist/some-repo
+Now lets check a multi-line comment, you should not see a link to https://github.com/b-zebrowski/1-deep
 */
-/* Another multi-line comment, you should not see a link to https://github.com/accountnotexist/some-repo */
+/* Another multi-line comment, you should not see a link to https://github.com/b-zebrowski/1-deep */
 this line you should see pointing towards https://github.com/b-zebrowski/1-deep
 
 this line should not show up as it ties back to a specific tag https://github.com/b-zebrowski/1-deep@123456
